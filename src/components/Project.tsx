1fb03e511917a5dcb1b40430d2acf8221da91bd7
Kelly Hum <kellyhum88@gmail.com>
2024-06-16 18:59:38 -0700

    initial html skeleton layout

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,3 +1,15 @@
-export default function Project() {
-    return <div>test</div>;
+type ProjectData = {
+    title: string;
+    tech: string[];
+    desc: string;
+};
+
+export default function Project({ title, tech, desc }: ProjectData) {
+    return (
+        <div className="project">
+            <h3>{title}</h3>
+            <div>{tech}</div>
+            <div>{desc}</div>
+        </div>
+    );
 }
