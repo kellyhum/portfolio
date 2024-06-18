d413bcd5d6248550f2d2dcce751c790caea88bbc
Kelly Hum <kellyhum88@gmail.com>
2024-06-18 12:30:39 -0700

    add params for past/present, conditional render for hero section

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,13 +1,41 @@
 type HeadingProps = {
     title: string;
     description: string;
+    hero?: boolean;
+    past?: string[];
+    present?: string[];
 };
 
-export default function Heading({ title, description }: HeadingProps) {
+export default function Heading({
+    title,
+    description,
+    hero,
+    past,
+    present,
+}: HeadingProps) {
     return (
         <>
             <h1>{title}</h1>
-            <div>{description}</div>
+
+            {hero ? (
+                <>
+                    <div className="hero-desc">
+                        As of late...
+                        {present?.map((bullet) => (
+                            <li>{bullet}</li>
+                        ))}
+                    </div>
+
+                    <div className="hero-desc">
+                        In past eras I...
+                        {past?.map((bullet) => (
+                            <li>{bullet}</li>
+                        ))}
+                    </div>
+                </>
+            ) : (
+                <div>{description}</div>
+            )}
         </>
     );
 }
