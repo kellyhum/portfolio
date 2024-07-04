4c804d07393bcb4c9c035ef78a1f549b645ed664
Kelly Hum <kellyhum88@gmail.com>
2024-07-03 21:07:28 -0700

    map data objects to about section (refactor about section)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Nav from "./components/Nav";
 import Project from "./components/Project";
+import AboutSection from "./components/AboutSection";
 
+import aboutData from "./data/about.json";
 import projectData from "./data/projects.json";
 
 import "./App.css";
@@ -14,7 +16,14 @@ export default function App() {
                 <h1>Hi, I'm Kelly</h1>
             </section>
 
+            <section id="about">
+                {aboutData.about.map((item) => (
+                    <AboutSection title={item.title} desc={item.desc} />
+                ))}
+            </section>
+
             <section id="projects">
+                <h1>Projects</h1>
                 <div id="project-container">
                     {projectData.projects.map((project) => (
                         <Project
