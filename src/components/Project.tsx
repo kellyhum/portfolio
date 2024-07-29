a03d88497d770397b52251e732f0598837bc35ec
Kelly Hum <kellyhum88@gmail.com>
2024-07-29 16:59:12 -0700

    renamed to tag

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,4 +1,4 @@
-import TechTag from "./TechTag";
+import Tag from "./Tag";
 
 type ProjectData = {
     title: string;
@@ -12,7 +12,7 @@ export default function Project({ title, tech, desc }: ProjectData) {
             <h3>{title}</h3>
             <div className="project-tech-container">
                 {tech.map((item) => (
-                    <TechTag name={item} />
+                    <Tag name={item} />
                 ))}
             </div>
             <div>{desc}</div>
