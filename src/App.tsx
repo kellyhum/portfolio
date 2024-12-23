c2a995551f27fb6f86b41f582b3d8f5240a10a27
Kelly Hum <kellyhum88@gmail.com>
2024-12-23 11:28:38 -0800

    switch card structure for redesign

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,58 @@
 import Nav from "./components/Nav";
-import "./App.css";
+import Card from "./components/Card";
+import "./styles/App.css";
+
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+import { CardInterface } from "./types";
+
+const currentCards: CardInterface[] = [
+    // array of cards
+    {
+        img: "",
+        tag: "project",
+        title: "Project Title",
+        desc: "project 1",
+    },
+    {
+        tag: "side hobby",
+        title: "Project Title",
+        desc: "testing no image",
+    },
+];
 
 export default function App() {
     return (
         <>
             <Nav />
 
-            {/* <footer>
-                <hr />
-                <div>Kelly Hum, copyright 2024</div>
-            </footer> */}
+            <main>
+                <div id="landingHeading">
+                    <h1>Kelly Hum</h1>
+                    <p>
+                        2nd year Computer Science student, aspiring keyboard
+                        wizard, and software engineer
+                    </p>
+
+                    <FaLinkedin />
+                    <FaGithub />
+                </div>
+
+                <div id="landingCurrent">
+                    <h2>Current</h2>
+                    <div id="landingCurrentCards">
+                        {currentCards.map((item) => (
+                            <Card
+                                img={item.title}
+                                tag={item.tag}
+                                title={item.title}
+                                desc={item.desc}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </main>
         </>
     );
 }
