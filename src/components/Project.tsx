1fbba896fcdce75a4fcb812b8682e032dac0c617
Kelly Hum <kellyhum88@gmail.com>
2025-01-26 18:01:59 -0800

    add link and do some minor styling

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,25 +1,34 @@
-import { Card, Image, Text } from "@chakra-ui/react";
-import ProjectImg from "../assets/card.jpg";
-
+import { Card, Image, Text, Link } from "@chakra-ui/react";
 import { ProjectInterface } from "../types";
 
-export default function Project({ tag, title, desc }: ProjectInterface) {
+export default function Project({ title, desc, link, img }: ProjectInterface) {
     return (
         <Card.Root
             maxW="sm"
             overflow="hidden"
             _hover={{ boxShadow: "0 0 8px 0 rgba(255, 255, 255, 0.25)" }}
         >
-            <Image src={ProjectImg} />
+            <Image src={img} maxH={375} />
             <Card.Body gap="2">
                 <Text
                     fontFamily="mono"
                     textTransform="uppercase"
                     fontSize="smallestSize"
                 >
-                    {tag}
+                    Project
                 </Text>
-                <Card.Title fontSize="subHeadingSize">{title}</Card.Title>
+                <Card.Title fontSize="subHeadingSize">
+                    <Link
+                        href={link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        _hover={{
+                            color: "#FBA600",
+                        }}
+                    >
+                        {title}
+                    </Link>
+                </Card.Title>
                 <Card.Description fontSize="bodySize">{desc}</Card.Description>
             </Card.Body>
         </Card.Root>
