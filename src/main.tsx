8fe1ba04c0019bd6f095e935814ab4012cb08eea
Kelly Hum <kellyhum88@gmail.com>
2025-01-26 18:26:25 -0800

    deploy and add analytics (vercel)

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "./components/ui/provider";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <Provider>
+            <Analytics />
             <App />
         </Provider>
     </React.StrictMode>
