3269b772e91b0563d9137ee01c2ce90a21ab6edd
Kelly Hum <kellyhum88@gmail.com>
2025-01-26 18:01:18 -0800

    small styling changes; add experience/project descriptions

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ import Project from "./components/Project";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 
-import AvatarImg from "./assets/card.jpg";
+import nwplus from "./assets/nwPlusLogo.png";
+import utg from "./assets/utg.jpeg";
+import testProjImg1 from "./assets/card.jpg";
+import testProjImg2 from "./assets/card2.jpg";
+import testProjImg3 from "./assets/card3.jpg";
 
-import { ProjectInterface } from "./types";
+import { ExperienceInterface, ProjectInterface } from "./types";
 
 import {
     AccordionItem,
@@ -17,7 +21,6 @@ import {
     Box,
     Separator,
     Link,
-    VStack,
     Text,
 } from "@chakra-ui/react";
 import { Avatar } from "../src/components/ui/avatar";
@@ -26,37 +29,51 @@ import StyledHeading from "./components/StyledHeading";
 const projects: ProjectInterface[] = [
     // array of cards
     {
-        tag: "project",
-        title: "Project Title",
-        desc: "project 1",
+        title: "Pokedash",
+        desc: "A Pokémon dashboard to track cards, values, and recommendations",
+        link: "https://github.com/kellyhum/pokedashboard",
+        img: testProjImg3,
     },
     {
-        tag: "side hobby",
-        title: "Project Title",
-        desc: "testing no image",
+        title: "Syntaxer",
+        desc: "A grammar checker for paragraphs written in Chinese",
+        link: "https://github.com/kellyhum/syntaxer",
+        img: testProjImg2,
     },
     {
-        tag: "side hobby",
-        title: "Project Title",
-        desc: "testing no image",
-    },
-    {
-        tag: "side hobby",
-        title: "Project Title",
-        desc: "testing no image",
+        title: "QuizLingo",
+        desc: "A platform to generate Duolingo-style quizzes for language learning",
+        link: "https://github.com/kellyhum/quizlingo",
+        img: testProjImg1,
     },
 ];
 
-const items = [
-    { value: "a", title: "First Item", text: "blah" },
-    { value: "b", title: "Second Item", text: "blah blah" },
-    { value: "c", title: "Third Item", text: "blah blah blah" },
+const experiences: ExperienceInterface[] = [
+    {
+        value: "a",
+        title: "Engagement Coordinator",
+        text: "Planning the retreats, internal socials, that nwPlus is known for! Also supporting some of Western Canada's largest hackathons",
+        org: "nwPlus UBC",
+        link: "http://nwplus.io/",
+        img: nwplus,
+        date: "May 2024 - Present",
+    },
+    {
+        value: "b",
+        title: "Coding Instructor",
+        text: "Teaching students how to code via games and projects ",
+        org: "Under the GUI Academy",
+        link: "http://nwplus.io/",
+        img: utg,
+        date: "January 2023 - Present",
+    },
 ];
 
 export default function App() {
     return (
         <Box
-            px="10"
+            px="20"
+            py="5"
             h="100%"
             height="100vh"
             width="100vw"
@@ -68,13 +85,17 @@ export default function App() {
             <Box pt={10} id="about">
                 <StyledHeading title="Kelly Hum" />
                 <Text>
-                    2nd year Computer Science student, aspiring keyboard wizard,
-                    and software engineer
+                    2nd year Computer Science student curious about software
+                    engineering and AI safety
+                </Text>
+                <Text mt={2}>
+                    When I'm not doing any of the above, I'll be reading,
+                    playing Catan, or collecting Pokémon cards
                 </Text>
 
                 <Flex gap="2" mt="4">
                     <Link
-                        href="https://youtube.com"
+                        href="https://www.linkedin.com/in/kellyhum/"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
@@ -82,7 +103,7 @@ export default function App() {
                     </Link>
 
                     <Link
-                        href="https://youtube.com"
+                        href="http://github.com/kellyhum"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
@@ -93,12 +114,13 @@ export default function App() {
 
             <Box my={20} id="projects">
                 <StyledHeading title="Projects" />
-                <Flex gap="5">
+                <Flex gap="5" mdDown={{ flexWrap: "wrap" }}>
                     {projects.map((project) => (
                         <Project
-                            tag={project.tag}
                             title={project.title}
                             desc={project.desc}
+                            link={project.link}
+                            img={project.img}
                         />
                     ))}
                 </Flex>
@@ -107,27 +129,54 @@ export default function App() {
             <Box my={20} id="experience">
                 <StyledHeading title="Experience" />
                 <AccordionRoot collapsible>
-                    {items.map((item) => (
+                    {experiences.map((item) => (
                         <AccordionItem value={item.value} mb={2}>
                             <AccordionItemTrigger cursor="pointer">
-                                <Avatar shape="square" src={AvatarImg} />
-                                <VStack gap={1}>
-                                    <Text
-                                        textAlign="left"
-                                        fontSize="subHeadingSize"
+                                <Avatar
+                                    shape="square"
+                                    src={item.img}
+                                    p={0}
+                                    m={0}
+                                />
+                                <Flex w="100%" justifyContent="space-between">
+                                    <Box
+                                        display="flex"
+                                        flexDirection="column"
+                                        gap={1}
                                     >
-                                        {item.title}
-                                    </Text>
-                                    <Text
-                                        fontSize="smallestSize"
-                                        color="fg.muted"
-                                    >
-                                        Year/date here
-                                    </Text>
-                                </VStack>
+                                        <Text
+                                            textAlign="left"
+                                            fontSize="subHeadingSize"
+                                        >
+                                            {item.title}
+                                        </Text>
+                                        <Text
+                                            textAlign="left"
+                                            // fontSize="smallestSize"
+                                        >
+                                            <Link
+                                                href={item.link}
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                color="fg.muted"
+                                                _hover={{
+                                                    color: "#FBA600",
+                                                }}
+                                            >
+                                                {item.org}
+                                            </Link>
+                                        </Text>
+                                    </Box>
+
+                                    <Box>{item.date}</Box>
+                                </Flex>
                             </AccordionItemTrigger>
 
-                            <AccordionItemContent fontSize="bodySize">
+                            <AccordionItemContent
+                                fontSize="bodySize"
+                                marginLeft={51}
+                                marginBottom={30}
+                            >
                                 {item.text}
                             </AccordionItemContent>
                         </AccordionItem>
@@ -137,7 +186,7 @@ export default function App() {
 
             <Box pb={8}>
                 <Flex align="center">
-                    <Box flexBasis="15%">Kelly Hum, 2025</Box>
+                    <Box flexBasis="15%">Kelly Hum, 2025 </Box>
                     <Separator flexBasis="auto"></Separator>
                 </Flex>
             </Box>
